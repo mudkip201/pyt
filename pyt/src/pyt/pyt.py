1b49c64ae72ff175ffc747dcc3aa3fab1fe89896
Matthew Cowen-Green <mudkipcg@yahoo.om>
2018-01-02 09:32:11 -0500

    Added capability to handle files encoded using Pyt's codepage

--- pyt/src/pyt/pyt.py
+++ pyt/src/pyt/pyt.py
@@ -12,11 +12,24 @@ def parse(line):
 def main():
     parser=argparse.ArgumentParser(description="Executes a pyt file")
     parser.add_argument("--file","-f",type=str,required=True)
+    parser.add_argument("--bytecode","-b",action="store_true")
     args=parser.parse_args()
+    
 
-    f=io.open(args.file,mode="r",encoding="utf-8")
-    parse(f.read())
-    f.close()
+    if(args.bytecode):
+        codepage=u"⁰¹²³⁴⁵⁶⁷⁸⁹₀₁₂₃₄₅₆₇₈₉¼½¾⅐⅑⅒⅓⅕⅖⅗⅘⅙⅚⅛⅜⅝⅞⅟Ƨ°|!÷↑↓←↕↔⇹¬^«»≤≥<>=≠√∛∜∞∈~˜%/+-*△⬠⬡∧∨⊼⊽⊻⊙⌊⌈⎶‰×ÅÁąáɓÇČƇĆçč¢ćĉɔĐðḋ₫éǝḞᵮǤĦĨƖǰḶĻĽĹŁĿļɬłɫṀϺɯɳṔƤǷҎᑭ₽Ṗ₱ᒆṕƥṗƿϼҏᵱŘɽɾɹʀřŕṛŞŠŜŚȘşŝšŤŦȚťŧỤʊŽžµΠπƩφ≡‼`⦋⁺⁻₊₋0123456789⑴·"
+        f=io.open(args.file,mode="rb")
+        line=u""
+        byte=f.read(1)
+        while byte!="":
+            line+=codepage[ord(byte)]
+            byte=f.read(1)
+        f.close()
+        parse(line)
+    else:
+        f=io.open(args.file,mode="r",encoding="utf-8")
+        parse(f.read())
+        f.close()
 
 
 
